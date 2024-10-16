2c83f604fb88804ab89b9e20c7de6401398a5192
tamiz <tamilselvan28052001@gmail.com>
2024-10-16 21:15:56 +0530

    add aws-exports.ts

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,9 @@ import App from "./App.tsx";
 import "./index.css";
 import { Amplify } from "aws-amplify";
 import outputs from "../amplify_outputs.json";
+import awsconfig from './aws-exports'; // Adjust the path if necessary
+
+Amplify.configure(awsconfig);
 
 Amplify.configure(outputs);
 
